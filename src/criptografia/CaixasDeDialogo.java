f7f42581a2d1d5c0d5490257794a0ab19a95218d
Leandro Arivielo <arivieloleando02@gmail.com>
2019-12-04 00:09:50 -0300

    quaseNoPonto

---
+++ src/criptografia/CaixasDeDialogo.java
@@ -0,0 +1,24 @@
+/*
+import javax.swing.JOptionPane;
+
+import javax.swing.JOptionPane;
+
+JOptionPane.showInputDialog(null,
+				"32 caracteres = chave com 256 bits"
+						+ "\n24 caracteres = chave com 192 bits"
+						+ "\n16 caracteres = chave com 128 bits"
+						+ "\n Infomre uma Chave:", "Insira a chave",
+				JOptionPane.PLAIN_MESSAGE);
+
+resposta = JOptionPane.showInputDialog(null,
+					"Escolha a operação que deseja", "Encriptagem simétrica",
+					JOptionPane.PLAIN_MESSAGE, null, opcoes, "criptografar");
+
+mensagem = JOptionPane.showInputDialog(null,
+"Informe sua mensagem a ser encriptada:",
+"Insira a Mensagem", JOptionPane.PLAIN_MESSAGE);
+
+mensagem = JOptionPane.showInputDialog(null,
+"Informe sua mensagem a ser descriptada:",
+"Insira a Mensagem", JOptionPane.PLAIN_MESSAGE);
+*/
